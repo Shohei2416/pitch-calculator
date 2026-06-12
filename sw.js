77deb93061bd79281e6747c45a20efe1b1d94e47
Shohei2416 <nishihiro.s@kk-suntechno.co.jp>
2026-06-12 18:59:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // pitch.htmlを修正したら、ここを「v3」「v4」と書き換えてアップロードする
-const CACHE_NAME = 'conveyor-calc-v6'; 
+const CACHE_NAME = 'conveyor-calc-v7'; 
 const ASSETS = [
   './pitch.html',
   './manifest.json',
